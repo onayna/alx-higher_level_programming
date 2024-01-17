ab291ce195142444dd45213b5ee7edc451bfe2de
onayna <rkayaeomaima@gmail.com>
2024-01-17 15:21:24 -0800

    posinega

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+
+# Generate a random signed number
+number = random.randint(-10, 10)
+
+# Print the number
+print(number, end=' ')
+
+# Check if the number is positive, negative, or zero
+if number > 0:
+    print("is positive")
+elif number == 0:
+    print("is zero")
+else:
+    print("is negative")
